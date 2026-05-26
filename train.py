c6f79cd4793a6fe67c6738f4d67eed4def36c236
Vinay <iiith.vinay2025@gmail.com>
2026-05-26 10:55:21 +0530

    Re-factor: Re-organize repository and preserve version one model weights.

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
-# Loading a pre-trained YOLOv8 model!
-model = YOLO('yolov8n.pt')
+# Loading a pre-trained YOLOv8 model! We're now using the small-model instead of the nano-model!
+model = YOLO('yolov8s.pt')
 
 # Train the model on the custom data-set!
-results = model.train(data = 'data.yaml', epochs = 25, imgsz = 640, plots = True)
+# fdvtwo = Fabric detector version two!
+results = model.train(data = 'data.yaml', epochs = 25, imgsz = 640, batch = 16, plots = True, name = "fdvtwo")
